55697da97be4f3d7a7447a255d183c8fd12c972c
alper1616 <alper_pacaci@hotmail.com>
2023-01-18 23:27:18 -0500

    string name

--- src/SecondPackage/ThirdPage.java
+++ src/SecondPackage/ThirdPage.java
@@ -1,6 +1,12 @@
 package SecondPackage;
 
 public class ThirdPage {
-  
-  // add some code
+    public static void main(String[] args) {
+
+
+        String name = "alper";
+        System.out.println(name);
+
+        // add some code
+    }
 }
